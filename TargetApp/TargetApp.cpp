27fb93efe71b2035a24c0f3081a8ce21b031df12
Pavel P <pavlov.pavel@gmail.com>
2026-02-09 16:40:16 +0200

    Add tests to unique_ptr
     + use vs2022 toolset
     + cleanup

--- TargetApp/TargetApp.cpp
+++ TargetApp/TargetApp.cpp
@@ -1,23 +1,27 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-// TargetApp.cpp : Example application which will be debuggeed
+// TargetApp.cpp : Example application which will be debugged
 
 #include <iostream>
 #include <windows.h>
+#include <memory>
 #include "../../../src/symbol.h"
 
 int wmain(int argc, WCHAR* argv[])
 {
-    Symbol0* sym2_spy0 = new Symbol0("spy");
-    Symbol0 sym2_nylda0("NYLD.A");
-    Symbol0 sym2_unknown0;
+    Symbol0* sym0_spy = new Symbol0("spy");
+    std::unique_ptr<Symbol0> sym0_ptr(sym0_spy);
+    Symbol0 sym0_nylda0("NYLD.A");
+    Symbol0 sym0_unknown0;
 
-    Symbol1* sym2_spy1 = new Symbol1("spy");
-    Symbol1 sym2_nylda1("NYLD.A");
-    Symbol1 sym2_unknown1;
+    Symbol1* sym1_spy = new Symbol1("spy");
+    std::unique_ptr<Symbol1> sym1_ptr(sym1_spy);
+    Symbol1 sym1_nylda1("NYLD.A");
+    Symbol1 sym1_unknown1;
 
-    Symbol2* sym2_spy2 = new Symbol2("spy");
+    Symbol2* sym2_spy = new Symbol2("spy");
+    std::unique_ptr<Symbol2> sym2_ptr(sym2_spy);
     Symbol2 sym2_nylda2("NYLD.A");
     Symbol2 sym2_unknown2;
 
@@ -26,7 +30,8 @@ int wmain(int argc, WCHAR* argv[])
     std::vector<Symbol2> syms2 = { Symbol2("SPY"), Symbol2("aapl") };
 
     __debugbreak(); // program will stop here. Evaluate 'sym2_spy', 'sym2_nylda' and 'sym2_unknown' in the locals or watch window.
-    std::cout << "Test complete\n";
+
+    printf("Test complete\n");
 
     return 0;
 }
--- dll/dllmain.cpp
+++ dll/dllmain.cpp
@@ -11,7 +11,7 @@ CBacktestEngineCustomVisualizerModule _AtlModule;
 extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
 	hInstance;
-	return _AtlModule.DllMain(dwReason, lpReserved); 
+	return _AtlModule.DllMain(dwReason, lpReserved);
 }
 
 // Used to determine whether the DLL can be unloaded by OLE
